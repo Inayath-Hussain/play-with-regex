9cb591f83390255b98a369daacf6b02511cc64b5
Inayath <inayathh2@gmail.com>
2023-09-24 19:12:51 +0530

    Add: theme of web app

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{ts, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#ffffff',
+        'background': '#16061e',
+        'primary': '#982acf',
+        'secondary': '#250a33',
+        'accent': '#a034d5',
+      },
+    },
   },
   plugins: [],
 }
