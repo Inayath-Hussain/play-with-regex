ecce442ecfb1ee6eb9377a30aeddcc1e604a510a
Inayath <inayathh2@gmail.com>
2023-11-02 21:45:58 +0530

    Add: vite config to refer tailwind config in react

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    commonjsOptions: {
+      include: ['tailwind.config.js', 'node_modules/**']
+    }
+  },
+  optimizeDeps: {
+    include: ['tailwind-config']
+  },
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      '@': './src',
+      'tailwind-config': './tailwind.config.cjs',
+    },
+  },
 })
